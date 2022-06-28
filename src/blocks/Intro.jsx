4f2d51bdf81819984d7104f35f491528658556ba
Antonio Rosales Martínez <antoniorosalesmartinez@MacBook-Pro-de-Antonio.local>
2022-06-28 15:28:21 +0200

    feat intro

---
+++ src/blocks/Intro.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <section className="intro">
+      <h1>
+        <img src="./assets/logos/main_logo.png" alt="The Last of Us logo" />
+      </h1>
+      <p>Personal Non-Comercial project created by Antonio Rosales</p>
+      <p>
+        The last of us II is an adventure thriller with elements of survival
+        horror and third-person stealth action, explore the story and watch the
+        characters in this ruined world.
+      </p>
+    </section>
+  );
+};
+
+export default Intro;
