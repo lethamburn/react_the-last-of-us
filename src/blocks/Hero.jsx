2bce4862d84e787911a5fa9773ed1463f8add760
Antonio Rosales Martínez <antoniorosalesmartinez@MacBook-Pro-de-Antonio.local>
2022-07-01 10:20:14 +0200

    FEAT hero

--- src/blocks/Hero.jsx
+++ src/blocks/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import BottomNavigator from "../components/BottomNavigator";
 import TopNavigator from "../components/TopNavigator";
 import videoBg from "./videos/hero.mp4";
 const Hero = () => {
@@ -6,14 +7,11 @@ const Hero = () => {
     <section className="hero">
       <video src={videoBg} autoPlay loop muted />
       <TopNavigator />
-      <h5>Experience</h5>
-      <p>
-        The Last of Us: Part II starts five years after the finale of the first
-        part. Growing up, Ellie and Joel live in the town of Jackson, but there
-        is a sense of tension between the characters - Even they go on patrols
-        separately, soon Ellie decides to leave the settlement and goes in
-        search of a group of people who have made her very angry.
-      </p>
+    
+    <img src="./assets/logos/part2.webp" alt="The Last of Us Part II logo" className="hero__logo"/>
+      <p>After the events of the first game, Joel Miller (Troy Baker) confesses to his brother Tommy (Jeffrey Pierce) his responsibility in preventing the Fireflies attempting to find a cure for the Cordyceps fungus pandemic by saving Ellie (Ashley Johnson) from an operation that would have killed her. Four years later, Joel and Ellie have built a life in Jackson, Wyoming, though their relationship has become strained. While on patrol, Joel and Tommy rescue a stranger, Abby Anderson (Laura Bailey), from an Infected horde. They return to an outpost run by Abby's group, former Fireflies now part of the Washington Liberation Front (WLF), a militia group based in Seattle, Washington.</p>
+      <h3>Official trailer ►</h3>
+      <BottomNavigator />
     </section>
   );
 };
---
+++ src/components/BottomNavigator.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const BottomNavigator = () => {
+  return (
+    <div className="bottom_navigator">
+      <nav>
+        <ul>
+          <li>
+            <h3>Home</h3>
+          </li>
+          <li>
+            <h3>Story</h3>
+          </li>
+          <li>
+            <h3>Characters</h3>
+          </li>
+          <li>
+            <h3>Media</h3>
+          </li>
+        </ul>
+      </nav>
+      <nav>
+        <ul>
+            <li>
+                <img src="./assets/logos/twitter.png" alt="Twitter logo" />
+            </li>
+            <li>
+                <img src="./assets/logos/facebook.png" alt="Facebook logo" />
+            </li>
+            <li>
+                <img src="./assets/logos/linkedin.png" alt="Linkedin logo" />
+            </li>
+            <li>
+                <img src="./assets/logos/github.png" alt="GitHub logo" />
+            </li>
+            <li>
+                <img src="./assets/logos/youtube.png" alt="Youtube logo" />
+            </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default BottomNavigator;
